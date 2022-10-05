255322793cdc1e19256d1152d261ed3106bbbb70
nitika02 <garg.nitika1998@gmail.com>
2022-10-06 01:00:29 +0530

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from "./components/navbar"
+import Hero from "./components/hero"
+import Card from "./components/card"
+import Data from "./data"
 
 function App() {
+  const cards = Data.map(item => {
+    return (
+      <Card 
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      location={item.location}
+      title={item.title}
+      price={item.price}
+      openSpots={item.openSpots}
+     />
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar />
+    
+    <Hero />
+    <section className="cards-list">
+    {cards}
+    </section>
+    
     </div>
-  );
+  )
 }
 
 export default App;
